ccdfd9928d18de2c5ca82a45dbea9c9336c58896
mbuel <morris.buel@gmail.com>
2021-03-14 23:16:12 -0700

    styling theme complete, started work on School Projects

--- js/nav.js
+++ js/nav.js
@@ -1,6 +1,5 @@
 // Hack to get nav bar to close after clicking link in nav bar
 jQuery(document).ready(function($){
-	$('#navbarSupportedContent').prepend('<div id="menu-icon">Menu</div>');
 	$("#menu-icon").on("click", function(){
 		$("#navbarSupportedContent").slideToggle();
 		$(this).toggleClass("active");
