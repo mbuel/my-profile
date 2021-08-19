d711987d0d960f69fc7bec9bd9268f197eeeb63c
mbuel <morris.buel@gmail.com>
2021-08-19 15:22:21 -0700

    in process of adding content, and customizing content.

---
+++ js/portfolio.js
@@ -0,0 +1,32 @@
+const projects = [
+  {
+    section: "school projects",
+    data: [
+     {
+      projectTitle:'Airbnb news clone',
+      projectImage:'https://d33wubrfki0l68.cloudfront.net/604efd2df93ba400089a8e74/screenshot.png',
+      projectLink:'https://sad-yalow-2fc858.netlify.app/'
+     }
+    ]
+  },
+  {
+    section: "personal projects",
+    data: [
+     {
+      projectTitle:'Airbnb news clone',
+      projectImage:'https://d33wubrfki0l68.cloudfront.net/604efd2df93ba400089a8e74/screenshot.png',
+      projectLink:'https://sad-yalow-2fc858.netlify.app/'
+     }
+    ]
+  },
+  {
+    section: "work history",
+    data: [
+     {
+      projectTitle:'Airbnb news clone',
+      projectImage:'https://d33wubrfki0l68.cloudfront.net/604efd2df93ba400089a8e74/screenshot.png',
+      projectLink:'https://sad-yalow-2fc858.netlify.app/'
+     }
+    ]
+  }
+]
